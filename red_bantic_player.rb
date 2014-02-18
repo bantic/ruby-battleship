0cdfa00f35eea220a6408355b59bf0b0e4a4331e
Lauren <lpellsworth@gmail.com>
2014-02-17 23:37:45 -0500

    Sample boards 5 and 6

--- red_bantic_player.rb
+++ red_bantic_player.rb
@@ -62,11 +62,11 @@ class RedBanticPlayer
 
   def sample_board_5
     [
-      [4, 5, 5, :across],
-      [7, 0, 4, :down],
-      [2, 1, 3, :down],
-      [5, 8, 3, :across],
-      [1, 7, 2, :across],
+      [0, 8, 5, :across],
+      [6, 3, 4, :down],
+      [7, 7, 3, :down],
+      [9, 7, 3, :down],
+      [9, 4, 2, :down],
     ]
   end
 
@@ -76,7 +76,7 @@ class RedBanticPlayer
       [1, 6, 4, :down],
       [2, 1, 3, :down],
       [5, 8, 3, :across],
-      [8, 8, 2, :across],
+      [8, 9, 2, :across],
     ]
   end
 end
